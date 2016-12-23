c7b9ebdcd4be59ea757c2098932d1e070a3cf10a
Osama <m.osamabinomar@gmail.com>
2016-12-23 20:11:43 +0500

    few changes in the process of startup. and rename the fragment numbers

--- app/src/main/java/com/cryptopaths/cryptofm/startup/InitActivity.java
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/InitActivity.java
@@ -8,8 +8,8 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,20 +38,25 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 
 public class InitActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
-    private static final int RC_PICK_FILE_TO_SAVE_INTERNAL  =100;
-    private static final int RC_PERMISSION                  =101;
-    private int mFragmentNumber                             =0;
-    private static final String TAG                         ="InitActivity";
+    private static final int RC_PERMISSION   = 101;
+    private static final String TAG          = "InitActivity";
+    private static int FRAGMENT_ONE_NUMBER   = 0;
+    private static int FRAGMENT_TWO_NUMBER   = 1;
+    private static int FRAGMENT_THREE_NUMBER = 2;
 
-    private DatabaseHandler     mDatabaseHandler;
-    private ProgressDialog      mLoading;
-    private ThirdFragment       mThirdFragment;
+    static {
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
 
+    private DatabaseHandler mDatabaseHandler;
+    private ProgressDialog  mLoading;
+    private SecondFragment mSecondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
         //first of all check shared preferences
         SharedPreferences preferences   =getPreferences(Context.MODE_PRIVATE);
         boolean isNotFirstRun           =preferences.getBoolean("key",false);
@@ -65,80 +70,80 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
 
         }
         //add first fragment
-        FirstFragment fragment=new FirstFragment();
-        getSupportFragmentManager().beginTransaction().
-                setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left,
-                        R.anim.enter_from_left, R.anim.exit_to_right).
-                replace(R.id.fragment_frame_layout,fragment).
-                addToBackStack("first").
-                commit();
-
+        replaceFragment(FRAGMENT_ONE_NUMBER);
 
     }
+
     @ActionHandler(layoutResource = R.id.next_button)
     public void onNextButtonClick(View v){
-        switch (mFragmentNumber){
-            case 0:
                 EditText passwordEditText=
                         (EditText)findViewById(R.id.password);
                 CharSequence sequence=passwordEditText.getText();
                 if(isValidPassword(sequence)){
-                    Log.d("fragment","replacing fragmnet "+mFragmentNumber);
-                    //create encrypted database and set password of user choice
-                    //mDatabaseHandler=new DatabaseHandler(this,sequence.toString(),false);
-                    //change fragment to next fragment
 
                     if(checkPermissions()){
                         //first remove the logo image from activity
                         View logoImage=findViewById(R.id.logo_image);
                         ((ViewGroup)logoImage.getParent()).removeView(logoImage);
-                        ThirdFragment secondFragment=new ThirdFragment();
-                        getSupportFragmentManager().beginTransaction().
-                                setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left,
-                                        R.anim.enter_from_left, R.anim.exit_to_right).
-                                replace(R.id.fragment_frame_layout,secondFragment).
-                                addToBackStack("third").
-                                commit();
-                        //set fragment number to 1
-                        mFragmentNumber=1;
-                    }else{
-                        getPermissions();
+                        //replace fragment to second fragment
+                        replaceFragment(FRAGMENT_TWO_NUMBER);
 
+                    }else{
+                        // get read and write storage permission
+                        getPermissions();
                     }
 
                 }else{
                     passwordEditText.setError("password length should be greater than 3");
                 }
-                Log.d("fragment","replacing fragmnet "+mFragmentNumber);
-                break;
-            case 1:
 
 
-                break;
-            case 2:
-                //get selected directories
-                assert mThirdFragment!=null;
-                ArrayList<String> tmp=mThirdFragment.getAllSelectedPositions();
-                if(tmp.size()>0){
-                    //start the encryption activity as user selected directories
-                    Intent intent=new Intent(this,IntermediateActivity.class);
-                    intent.putExtra("dirs",tmp);
-                    //commit that all in set up activity is done well.
-                    commitInitActivity();
-                    //clear stack
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivityForResult(intent,0);
-                    finish();
-                }else {
-                    Toast.makeText(this,
-                            "Please choose one or more directories",
-                            Toast.LENGTH_LONG
-                    ).show();
-                }
 
+    }
+    @ActionHandler(layoutResource = R.id.lets_go_button)
+    public void onLetsGoButtonClick(View v){
+        //get selected directories
+        assert mSecondFragment !=null;
+        ArrayList<String> tmp= mSecondFragment.getAllSelectedPositions();
+        if(tmp.size()>0){
+            //change fragment to third fragment
+            replaceFragment(FRAGMENT_THREE_NUMBER);
+        }else {
+            Toast.makeText(
+                    this,
+                    "Please choose one or more directories",
+                    Toast.LENGTH_LONG
+            ).show();
         }
     }
-
+    private void replaceFragment(int fragmentNumber){
+        String fragmentBackName="";
+        Fragment fragment;
+        switch (fragmentNumber){
+            case 0:
+                fragmentBackName="first";
+                fragment=new FirstFragment();
+                break;
+            case 1:
+                fragmentBackName="second";
+                fragment=new SecondFragment();
+                // as I need it later
+                mSecondFragment= (SecondFragment) fragment;
+                break;
+            case 2:
+                fragmentBackName="third";
+                fragment=new ThirdFragment();
+                break;
+            default:
+                return;
+        }
+        getSupportFragmentManager().beginTransaction().
+                setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left,
+                        R.anim.enter_from_left, R.anim.exit_to_right).
+                replace(R.id.fragment_frame_layout, fragment).
+                addToBackStack(fragmentBackName).
+                commit();
+    }
 
 
     private void commitInitActivity() {
@@ -150,16 +155,6 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
         editor.commit();
     }
 
-    private void getPermissions() {
-        checkPermissions();
-    }
-
-
-
-
-    private boolean isValidEmail(CharSequence target) {
-        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
-    }
     private boolean isValidPassword(CharSequence password){
         return password.length() > 2;
     }
@@ -167,22 +162,17 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
     /*
     Permission area
      */
+    private void getPermissions() {
+        checkPermissions();
+    }
     @Override
     public void onPermissionsGranted(int requestCode, List<String> perms) {
         Log.d(TAG,"permissions granted");
         if (requestCode==RC_PERMISSION){
-            //first remove the logo image from activity
             View logoImage=findViewById(R.id.logo_image);
             ((ViewGroup)logoImage.getParent()).removeView(logoImage);
-            //change fragment to third fragment
-            mThirdFragment=new ThirdFragment();
-            getSupportFragmentManager().beginTransaction().
-                    setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left,
-                            R.anim.enter_from_left, R.anim.exit_to_right).
-                    replace(R.id.fragment_frame_layout,mThirdFragment).
-                    addToBackStack("third").
-                    commit();
-            mFragmentNumber=2;
+            //change fragment to second fragment
+            replaceFragment(FRAGMENT_TWO_NUMBER);
         }
     }
 
@@ -190,6 +180,7 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
     public void onPermissionsDenied(int requestCode, List<String> perms) {
 
     }
+
     @AfterPermissionGranted(RC_PERMISSION)
     private boolean checkPermissions(){
         String[] perms  ={Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -202,6 +193,7 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
         }
         return false;
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -209,6 +201,16 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     /*
@@ -269,7 +271,6 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
         protected void onPostExecute(byte[] s) {
             super.onPostExecute(s);
             mLoading.dismiss();
-            mFragmentNumber = 2;
             //make a toast the keys successfully generated
             Toast.makeText(InitActivity.this,
                     "Successfully generated keys",
@@ -280,18 +281,5 @@ public class InitActivity extends AppCompatActivity implements EasyPermissions.P
 
 
         }
-    }
-    static {
-        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 }
--- app/src/main/java/com/cryptopaths/cryptofm/startup/ThirdFragment.java
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/ThirdFragment.java
@@ -6,59 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.cryptopaths.cryptofm.R;
 
-import java.util.ArrayList;
-
 /**
  * Created by tripleheader on 11/28/16.
- *
  */
 
 public class ThirdFragment extends Fragment {
-    private ChooseDirAdapter mAdapter;
-    private ArrayList<String> allSelectedPositions=new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView= inflater.inflate(R.layout.third_fragment,container,false);
-        final ListView listView=(ListView)rootView.findViewById(R.id.choose_dir_list);
-        mAdapter=new ChooseDirAdapter(getActivity());
-        listView.setAdapter(mAdapter);
-        View footerView=inflater.inflate(R.layout.choose_file_footer,null,false);
-        listView.addFooterView(footerView);
-        // set item selected in filebrowse_lisrview by just a Tap
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(!isAlreadySelected(i)){
-                    allSelectedPositions.add("value"+i);
-
-                }else{
-                    allSelectedPositions.remove("value"+i);
-
-                }
-                mAdapter.setAllSelectedPositions(allSelectedPositions);
-                mAdapter.notifyDataSetChanged();
-
-            }
-        });
-        return rootView;
-    }
-    private boolean isAlreadySelected(int position){
-        for (String pos:
-             allSelectedPositions) {
-            if(pos.equals("value"+position)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public ArrayList<String> getAllSelectedPositions() {
-        return mAdapter.getmSelectedFoldersPaths();
+        return inflater.inflate(R.layout.second_fragment,null);
     }
 }
