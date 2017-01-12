db7407532646e24eac189173fd6fa0cd4b11428c
Shadow <hamzalatif72@gmail.com>
2017-01-12 21:00:36 +0500

    View pager skip button listener

--- app/src/main/java/com/cryptopaths/cryptofm/startup/PreStartActivity.java
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/PreStartActivity.java
@@ -1,11 +1,15 @@
 package com.cryptopaths.cryptofm.startup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.cryptopaths.cryptofm.R;
 import com.cryptopaths.cryptofm.startup.adapters.PagerAdapter;
+import com.cryptopaths.cryptofm.utils.ActionHandler;
 
 public class PreStartActivity extends AppCompatActivity {
     //app compat activity can also act as FragmentActivity
@@ -19,7 +23,16 @@ public class PreStartActivity extends AppCompatActivity {
         viewpager = (ViewPager) findViewById(R.id.pager);
         PagerAdapter padapter = new PagerAdapter(getSupportFragmentManager());
         viewpager.setAdapter(padapter);
-
-
     }
+
+    @ActionHandler(layoutResource = R.id.button)
+    Button button=(Button)findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Perform action on click
+        }
+    });
 }
+
+
