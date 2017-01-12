302f3a9b1596d91aca058a9daddb6baeb5241723
Shadow <hamzalatif72@gmail.com>
2017-01-12 18:57:55 +0500

    View pager Fragments added

--- app/src/main/java/com/cryptopaths/cryptofm/startup/PreStartActivity.java
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/PreStartActivity.java
@@ -1,15 +1,23 @@
 package com.cryptopaths.cryptofm.startup;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cryptopaths.cryptofm.R;
+import com.cryptopaths.cryptofm.startup.adapters.PagerAdapter;
 
 public class PreStartActivity extends AppCompatActivity {
+    //app compat activity can also act as FragmentActivity
+
+    ViewPager viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_start);
+        viewpager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter padapter = new PagerAdapter(getSupportFragmentManager());
+        viewpager.setAdapter(padapter);
     }
 }
