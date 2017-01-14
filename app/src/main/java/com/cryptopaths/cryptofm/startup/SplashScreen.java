1056784a3ebf94e36618e4453e353eea5b844183
patriot <patriot321power@gmail.com>
2017-01-14 15:37:22 +0500

    A simple splash screen that works with timer has been added

---
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.cryptopaths.cryptofm.startup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.*;
+import android.content.Intent;
+import android.view.*;
+
+import com.cryptopaths.cryptofm.R;
+
+/**
+ * Created by patriot on 1/14/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 6000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // This method will be executed once the timer is over
+                        // Start your app main activity
+                        Intent i = new Intent(SplashScreen.this, InitActivity.class);
+                        startActivity(i);
+
+                        // close this activity
+                        finish();
+                    }
+                }, SPLASH_TIME_OUT);
+            }
+}
+
