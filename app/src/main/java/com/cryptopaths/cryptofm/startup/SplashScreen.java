5e7fd235a593707ef42be67624c98bc3bd7513c4
Osama <m.osamabinomar@gmail.com>
2017-01-16 19:45:41 +0500

    set the activities right

--- app/src/main/java/com/cryptopaths/cryptofm/startup/SplashScreen.java
+++ app/src/main/java/com/cryptopaths/cryptofm/startup/SplashScreen.java
@@ -1,29 +1,39 @@
 package com.cryptopaths.cryptofm.startup;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.*;
+import android.content.Context;
 import android.content.Intent;
-import android.view.*;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.cryptopaths.cryptofm.R;
 
 /**
  * Created by patriot on 1/14/17.
+ * Splash activity which just starts another activity
  */
 
 public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                        // This method will be executed once the timer is over
-                        // Start your app main activity
-                        Intent i = new Intent(SplashScreen.this, InitActivity.class);
-                        startActivity(i);
+        SharedPreferences preferences=getPreferences(Context.MODE_PRIVATE);
+        boolean isFirstRun=preferences.getBoolean("run",true);
+        if(isFirstRun){
+            SharedPreferences.Editor editor=preferences.edit();
+            editor.putBoolean("run",false);
+            editor.apply();
+            editor.commit();
+            Intent i = new Intent(SplashScreen.this, PreStartActivity.class);
+            startActivity(i);
 
-                        // close this activity
-                        finish();
+        }else{
+            Intent i = new Intent(SplashScreen.this, InitActivity.class);
+            startActivity(i);
+        }
+        // close this activity
+        finish();
 
 
-            }
+    }
 }
 
