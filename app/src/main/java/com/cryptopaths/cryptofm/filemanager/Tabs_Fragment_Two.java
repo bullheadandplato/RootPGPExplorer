b0addb54437f9411f175e722dfaec8ba93f0518d
Shadow <hamzalatif72@gmail.com>
2017-01-21 21:58:24 +0500

    file manager tabs

---
+++ app/src/main/java/com/cryptopaths/cryptofm/filemanager/Tabs_Fragment_Two.java
@@ -0,0 +1,23 @@
+package com.cryptopaths.cryptofm.filemanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cryptopaths.cryptofm.R;
+
+/**
+ * Created by Shadow on 1/21/2017.
+ */
+
+public class Tabs_Fragment_Two extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tabs_fragment_two,container,false);
+    }
+}
