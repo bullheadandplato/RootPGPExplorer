ecacd5ec3ecb7a158cbd6dade5e50c5aec7a5204
Osama <m.osamabinomar@gmail.com>
2017-02-03 00:31:29 +0500

    refactor

--- app/src/main/java/com/cryptopaths/cryptofm/encryption/KeyOperations.java
+++ app/src/main/java/com/cryptopaths/cryptofm/encryption/KeyOperations.java
@@ -2,18 +2,14 @@ package com.cryptopaths.cryptofm.encryption;
 
 import org.spongycastle.openpgp.PGPKeyRingGenerator;
 import org.spongycastle.openpgp.PGPPublicKey;
-import org.spongycastle.openpgp.PGPSecretKey;
 
 import java.io.File;
-import java.io.InputStream;
 
 /**
  * Created by osama on 10/8/16.
+ *
  */
 public interface KeyOperations  {
-    public PGPPublicKey getPublicKey(File binaryData)throws Exception;
-    public PGPSecretKey getSecretKey(InputStream binaryData, long keyID) throws Exception;
-    public PGPKeyRingGenerator generateKey(String email, char[] password) throws Exception;
-
-
+    PGPPublicKey getPublicKey(File binaryData)throws Exception;
+    PGPKeyRingGenerator generateKey(String email, char[] password) throws Exception;
 }
