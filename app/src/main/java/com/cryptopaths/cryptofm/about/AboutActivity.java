9a5e2cf971ae4572b239ab253e8d6600542af8da
Osama <m.osamabinomar@gmail.com>
2017-02-15 19:56:31 +0500

    licenses are completed and tested

--- app/src/main/java/com/cryptopaths/cryptofm/about/AboutActivity.java
+++ app/src/main/java/com/cryptopaths/cryptofm/about/AboutActivity.java
@@ -1,7 +1,9 @@
 package com.cryptopaths.cryptofm.about;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.cryptopaths.cryptofm.R;
 
@@ -11,5 +13,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    public void onLicenseClick(View view){
+        startActivity(new Intent(this,LicencesActivity.class));
     }
 }
