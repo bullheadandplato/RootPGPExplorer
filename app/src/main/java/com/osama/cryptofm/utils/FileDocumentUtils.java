161629ba9fd61755241697e809c6eb55fcff3d04
tripleheader <m.osamabinomar@gmail.com>
2017-02-17 20:02:02 +0500

    what slipped!!

--- app/src/main/java/com/osama/cryptofm/utils/FileDocumentUtils.java
+++ app/src/main/java/com/osama/cryptofm/utils/FileDocumentUtils.java
@@ -100,7 +100,7 @@ public class FileDocumentUtils {
 
 
     public static DocumentFile getDocumentFile(final File file) {
-        String baseFolder = SharedData.EXTERNAL_SDCARD_ROOT_PATH;
+        String baseFolder = SharedData.EXTERNAL_SDCARD_ROOT_PATH+"/";
         boolean isDirectory=file.isDirectory();
         if (baseFolder == null) {
             return null;
