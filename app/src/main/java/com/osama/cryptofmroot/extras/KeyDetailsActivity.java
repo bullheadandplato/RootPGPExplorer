24aa6df616648d8d185f9429848e94f0389994d9
Osama Bin Omar <m.osamabinomar@gmail.com>
2017-05-13 23:59:05 +0500

    key details activity

---
+++ app/src/main/java/com/osama/cryptofmroot/extras/KeyDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.osama.cryptofmroot.extras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.osama.cryptofmroot.R;
+
+public class KeyDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_details);
+    }
+}
